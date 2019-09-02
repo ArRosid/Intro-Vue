203aa6cf350c0b40e9b738155f95268507c861ea
ArRosid <ahmadrosid30121997@gmail.com>
2019-09-03 05:11:55 +0700

    First Vue Instance

---
+++ 01-First-Vue-Instance/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        message: "Hello World!",
+        value: 5,
+        imgSrc: "https://miro.medium.com/max/1024/1*j_f-fL9EHgaFn4IRi7NZ7g.png",
+        link: "https://vuejs.org"
+    }
+})
